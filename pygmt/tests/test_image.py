c61bbfab0fd037d88a26ee12934d8f286e811575
Dongdong Tian <seisman.info@gmail.com>
2020-07-11 23:55:27 -0400

    Debug test_image

--- pygmt/tests/test_image.py
+++ pygmt/tests/test_image.py
@@ -15,5 +15,5 @@ TEST_IMG = os.path.join(os.path.dirname(__file__), "baseline", "test_logo.png")
 def test_image():
     "Place images on map"
     fig = Figure()
-    fig.image(TEST_IMG, D="x0/0+w1i", F="+pthin,blue")
+    fig.image(TEST_IMG, D="x0/0+w1i", F="+pthin,blue", V='d')
     return fig
